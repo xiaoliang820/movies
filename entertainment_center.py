c40023b51e5df2481815b841651a29cfdd565d74
xiaoliang820 <jerrymakou@gmail.com>
2016-09-28 10:55:21 +0800

    Fix issues according to the code review

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,41 +1,42 @@
 import media
 import fresh_tomatoes
 
+# Create some Movie instances:
 toy_story = media.Movie("Toy Story",
                         "A story of a boy and his toys that come to life",
-                        "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
+                        "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",  # noqa
                         "https://www.youtube.com/watch?v=vwyZH85NQC4")
 
 avatar = media.Movie("Avatar",
                      "A marine on an alien planet",
-                     "http://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg",
-                     "https://www.youtube.com/watch?v=5PSNL1qE6VY"
-                     )
+                     "http://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg",  # noqa
+                     "https://www.youtube.com/watch?v=5PSNL1qE6VY")
 
 school_of_rock = media.Movie("School of Rock",
                              "Storyline",
-                             "http://upload.wikimedia.org/wikipedia/en/1/11/School_of_Rock_Poster.jpg",
+                             "http://upload.wikimedia.org/wikipedia/en/1/11/School_of_Rock_Poster.jpg",    # noqa
                              "https://www.youtube.com/watch?v=3PsUJFEBC74")
 
 
 ratatouille = media.Movie("Ratatouille",
-                     "Storyline",
-                     "http://upload.wikimedia.org/wikipedia/en/5/50/RatatouillePoster.jpg",
-                     "https://www.youtube.com/watch?v=c3sBBRxDAqk"
-                     )
+                          "Storyline",
+                          "http://upload.wikimedia.org/wikipedia/en/5/50/RatatouillePoster.jpg",    # noqa
+                          "https://www.youtube.com/watch?v=c3sBBRxDAqk")
 
 midnight_in_paris = media.Movie("Midnight in Paris",
-                     "Storyline",
-                     "http://upload.wikimedia.org/wikipedia/en/9/9f/Midnight_in_Paris_Poster.jpg",
-                     "https://www.youtube.com/watch?v=atLg2wQQxvU"
-                     )
+                                "Storyline",
+                                "http://upload.wikimedia.org/wikipedia/en/9/9f/Midnight_in_Paris_Poster.jpg",   # noqa
+                                "https://www.youtube.com/watch?v=atLg2wQQxvU")
 
 hunger_games = media.Movie("Hunger Games",
-                     "Storyline",
-                     "http://upload.wikimedia.org/wikipedia/en/4/42/HungerGamesPoster.jpg",
-                     "https://www.youtube.com/watch?v=PbA63a7H0bo"
-                     )
+                           "Storyline",
+                           "http://upload.wikimedia.org/wikipedia/en/4/42/HungerGamesPoster.jpg",   # noqa
+                           "https://www.youtube.com/watch?v=PbA63a7H0bo")
 
-movies = [toy_story, avatar, school_of_rock, ratatouille, midnight_in_paris, hunger_games]
-fresh_tomatoes.open_movies_page(movies) 
+# Add movies to a list:
+movies = [toy_story, avatar, school_of_rock, ratatouille,
+          midnight_in_paris, hunger_games]
+
+# This function creates a website that shows the movies.
+fresh_tomatoes.open_movies_page(movies)
 
--- media.py
+++ media.py
@@ -1,12 +1,22 @@
 import webbrowser
 
+
 class Movie():
-    VALID_RATINGS= ["G", "PG", "PG-13", "R"]
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+    """
+    The Movie class is used to store your favorite movies.
+    """
+    def __init__(self, movie_title, movie_storyline,
+                 poster_image, trailer_youtube):
+    	"""
+        The initializer of Movie class
+        """
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
     def show_trailer(self):
+    	"""
+        Play the movie trailer in your browser
+        """
         webbrowser.open(self.trailer_youtube_url)
